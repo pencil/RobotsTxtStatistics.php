e0c0bddf8e9a06dffc7c6ae12ad4fcbf1a3886c9
Nils Caspar <ncaspar@me.com>
2012-05-26 17:01:59 +0200

    fixed naming

--- stats.php
+++ stats.php
@@ -5,7 +5,7 @@ header('Content-Type: text/plain');
 require_once 'lib/Model.php';
 $format = '%-25s %-40s %20s' . "\n";
 
-printf($format, 'BROWSER', 'LAST REQUEST', 'TOTAL REQUESTS');
+printf($format, 'NAME', 'LAST REQUEST', 'TOTAL REQUESTS');
 foreach(Model::getTop(25) as $top) {
   printf($format, $top['name'], date('c', $top['last_request']), $top['total_requests']);
 }
